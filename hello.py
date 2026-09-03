3037fd82e5b7c8accdf6576722eb9d91bd82f14b
subeerrakeshgupta <subeergupta98@gmail.com>
2026-09-03 21:41:20 +0530

    Add two programs for sum and area calculations

--- hello.py
+++ hello.py
@@ -1,7 +1,24 @@
-Ques: Write a program to input 2 numbers & print their sum ?
+'''
+Ques 1: Write a program to input 2 numbers & print their sum ?
+'''
+
 first = int(input("enter first:"))
 second = int(input("enter second:"))
 print("sum=",first+second)
+
+'''
  result :enter first:5
-enter second:4
-sum= 9
+         enter second:4
+         sum= 9
+'''
+'''
+Ques 2 :Write a program to input side of a square & print its area ?
+'''
+
+side = float(input("enter square side:"))
+print("area = ",side*side)
+
+'''
+result :enter square side:5
+area =  25.0
+'''
